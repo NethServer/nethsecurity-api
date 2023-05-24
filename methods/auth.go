db52ca68fe11ae22b83cef740b34a3bddeea2804
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-05-24 11:07:43 +0200

    LICENSE. change license type

--- methods/auth.go
+++ methods/auth.go
@@ -2,20 +2,7 @@
  * Copyright (C) 2023 Nethesis S.r.l.
  * http://www.nethesis.it - info@nethesis.it
  *
- * This file is part of NethServer project.
- *
- * NethServer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or any later version.
- *
- * NethServer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with NethServer.  If not, see COPYING.
+ * SPDX-License-Identifier: GPL-2.0-only
  *
  * author: Edoardo Spadoni <edoardo.spadoni@nethesis.it>
  */
