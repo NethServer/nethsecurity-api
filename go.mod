3b2e3a0823c9057dc0ac9090b246045e1ff9a4a8
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-05-24 15:30:16 +0200

    ubus. added POST call action

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/NethServer/ns-api-server
 go 1.16
 
 require (
+	github.com/Jeffail/gabs/v2 v2.7.0 // indirect
 	github.com/NethServer/ns8-core/core/api-server v0.0.0-20230511093202-c2f91171c039
 	github.com/appleboy/gin-jwt/v2 v2.9.1
 	github.com/dgryski/dgoogauth v0.0.0-20190221195224-5a805980a5f3
--- methods/ubus.go
+++ methods/ubus.go
@@ -9,8 +9,55 @@
 
 package methods
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"net/http"
+	"os/exec"
+
+	"github.com/NethServer/ns-api-server/models"
+	"github.com/NethServer/ns-api-server/response"
+
+	"github.com/Jeffail/gabs/v2"
+	"github.com/fatih/structs"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
 
 func UBusCallAction(c *gin.Context) {
+	// parse request fields
+	var jsonUBusCall models.UBusCallJSON
+	if err := c.ShouldBindBodyWith(&jsonUBusCall, binding.JSON); err != nil {
+		c.JSON(http.StatusBadRequest, structs.Map(response.StatusBadRequest{
+			Code:    400,
+			Message: "request fields malformed",
+			Data:    err.Error(),
+		}))
+		return
+	}
 
+	// convert payload to JSON
+	jsonPayload, _ := json.Marshal(jsonUBusCall.Payload)
+
+	// execute login command on ubus
+	out, err := exec.Command("/bin/ubus", "-S", "call", jsonUBusCall.Path, jsonUBusCall.Method, string(jsonPayload[:])).Output()
+
+	// parse output in a valid JSON
+	jsonParsed, _ := gabs.ParseJSON(out)
+
+	// check errors
+	if err != nil {
+		c.JSON(http.StatusBadRequest, structs.Map(response.StatusBadRequest{
+			Code:    400,
+			Message: "[UBUS] call action failed",
+			Data:    err.Error(),
+		}))
+		return
+	}
+
+	// return 200 OK with data
+	c.JSON(http.StatusOK, structs.Map(response.StatusOK{
+		Code:    200,
+		Message: "[UBUS] call action success",
+		Data:    jsonParsed,
+	}))
 }
