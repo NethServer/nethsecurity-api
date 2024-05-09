7b7d3ffe899c8d35f0fb5df722149e089e684b12
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-05-09 11:20:34 +0200

    improve check

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -135,7 +135,11 @@ func InitJWT() *jwt.GinJWTMiddleware {
 
 			// extract body
 			reqBody := ""
-			if reqMethod == "POST" || reqMethod == "PUT" {
+			// if reqURI contains /files path, just replace the body with a static string to avoid logging the file content
+			if strings.Contains(reqURI, "/files") {
+				reqBody = "<file>"
+			}
+			if reqBody != "<file>" && (reqMethod == "POST" || reqMethod == "PUT") {
 				// extract body
 				var buf bytes.Buffer
 				tee := io.TeeReader(c.Request.Body, &buf)
@@ -161,10 +165,6 @@ func InitJWT() *jwt.GinJWTMiddleware {
 
 				// compose req body
 				reqBody = jsonB
-			}
-			// if reqURI contains /files path, just replace the body with a static string to avoid logging the file content
-			if strings.Contains(reqURI, "/files") {
-				reqBody = "<file>"
 			}
 
 			logs.Logs.Println("[INFO][AUTH] authorization success for user " + claims["id"].(string) + ". " + reqMethod + " " + reqURI + " " + reqBody)
