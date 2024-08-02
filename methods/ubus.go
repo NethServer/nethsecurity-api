NethServer/nethsecurity-api

commit 2f2706bd629c2609d180ce356d39f189aa83e666

ubus: bypass rpcd when possible
Directly execute ns.<api> scripts to overcome
rpcd limits.

Security checks before executing the script:
- add the absolute base path /usr/libexec/rpcd: prevent
  execution for arbitrary scripts
- make sure the script already belong to a package: prevent
  execution of uploaded files

methods/ubus.go
@@ -11,11 +11,15 @@ package methods
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/NethServer/nethsecurity-api/models"
 	"github.com/NethServer/nethsecurity-api/response"
+	"github.com/NethServer/nethsecurity-api/utils"
 
 	"github.com/Jeffail/gabs/v2"
 	"github.com/fatih/structs"
@@ -23,9 +27,33 @@ import (
 	"github.com/gin-gonic/gin/binding"
 )
 
+// List, to check if path is allowed:
+// this is a security measure to avoid direct calls to binaries
+// that are not part of any package
+var validPaths []string
+
+func LoadValidPaths() {
+	validPaths = make([]string, 0)
+	files, err := os.ReadDir("/usr/libexec/rpcd")
+	if err != nil {
+		return
+	}
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), "ns.") {
+			path := "/usr/libexec/rpcd/" + file.Name()
+			// execute opkg search path, if output is empty, the file does not belong to any package: don't add to valid paths
+			out, err := exec.Command("/bin/opkg", "search", path).Output()
+			if out != nil && err == nil {
+				validPaths = append(validPaths, path)
+			}
+		}
+	}
+}
+
 func UBusCallAction(c *gin.Context) {
 	// parse request fields
 	var jsonUBusCall models.UBusCallJSON
+	var cmd *exec.Cmd
 	if err := c.ShouldBindBodyWith(&jsonUBusCall, binding.JSON); err != nil {
 		c.JSON(http.StatusBadRequest, structs.Map(response.StatusBadRequest{
 			Code:    400,
@@ -38,10 +66,42 @@ func UBusCallAction(c *gin.Context) {
 	// convert payload to JSON
 	jsonPayload, _ := json.Marshal(jsonUBusCall.Payload)
 
-	// execute login command on ubus
-	out, err := exec.Command("/bin/ubus", "-S", "-t", "300", "call", jsonUBusCall.Path, jsonUBusCall.Method, string(jsonPayload[:])).Output()
+	// check if path starts with ns.
+	if jsonUBusCall.Path[:3] == "ns." {
+		// force base path to avoid calling other system binaries
+		jsonUBusCall.Path = "/usr/libexec/rpcd/" + jsonUBusCall.Path
+		// check if path is inside valid paths list
+		if !utils.Contains(jsonUBusCall.Path, validPaths) {
+			c.JSON(http.StatusBadRequest, structs.Map(response.StatusBadRequest{
+				Code:    400,
+				Message: "invalid path",
+				Data:    jsonUBusCall.Path,
+			}))
+			return
+		}
+
+		cmd = exec.Command(jsonUBusCall.Path, "call", jsonUBusCall.Method)
+
+		// execute direct script call
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, structs.Map(response.StatusBadRequest{
+				Code:    500,
+				Message: "ubus call action failed",
+				Data:    err.Error(),
+			}))
+			return
+		}
+
+		io.WriteString(stdin, string(jsonPayload))
+		stdin.Close()
+	} else {
+		// fallback to rpcd
+		cmd = exec.Command("/bin/ubus", "-S", "-t", "300", "call", jsonUBusCall.Path, jsonUBusCall.Method, string(jsonPayload[:]))
+	}
 
 	// check errors
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, structs.Map(response.StatusBadRequest{
 			Code:    500,
@@ -52,7 +112,15 @@ func UBusCallAction(c *gin.Context) {
 	}
 
 	// parse output in a valid JSON
-	jsonParsed, _ := gabs.ParseJSON(out)
+	jsonParsed, err := gabs.ParseJSON(out)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, structs.Map(response.StatusBadRequest{
+			Code:    500,
+			Message: "invalid JSON response",
+			Data:    jsonParsed,
+		}))
+		return
+	}
 
 	// check errors in response
 	errorMessage, errFound := jsonParsed.Path("error").Data().(string)
@@ -60,7 +128,7 @@ func UBusCallAction(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, structs.Map(response.StatusBadRequest{
 			Code:    500,
 			Message: errorMessage,
-			Data: jsonParsed,
+			Data:    jsonParsed,
 		}))
 		return
 	}
@@ -71,7 +139,7 @@ func UBusCallAction(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, structs.Map(response.StatusBadRequest{
 			Code:    400,
 			Message: "validation_failed",
-			Data: jsonParsed,
+			Data:    jsonParsed,
 		}))
 		return
 	}

main.go
@@ -12,6 +12,9 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/fatih/structs"
 	"github.com/gin-contrib/cors"
@@ -46,6 +49,19 @@ func main() {
 
 	// init configuration
 	configuration.Init()
+
+	// load list of valid paths
+	methods.LoadValidPaths()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGUSR1)
+	go func() {
+		sig := <-sigs
+		if sig == syscall.SIGUSR1 {
+			logs.Logs.Println("Reloading valid path list")
+			methods.LoadValidPaths()
+		}
+	}()
 
 	// disable log to stdout when running in release mode
 	if gin.Mode() == gin.ReleaseMode {

commit 6095bfbf900381843507c9ad9e1bbc2cdf3e4654

ubus: ignore JSON parse error
The gabs library can sometimes fail to decode
a valid JSON.

Regression introduced by 2f2706bd629c2609d180ce356d39f189aa83e666

methods/ubus.go
@@ -114,15 +114,7 @@ func UBusCallAction(c *gin.Context) {
 	}
 
 	// parse output in a valid JSON
-	jsonParsed, err := gabs.ParseJSON(out)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, structs.Map(response.StatusBadRequest{
-			Code:    500,
-			Message: "invalid JSON response",
-			Data:    jsonParsed,
-		}))
-		return
-	}
+	jsonParsed, _ := gabs.ParseJSON(out)
 
 	// check errors in response
 	errorMessage, errFound := jsonParsed.Path("error").Data().(string)